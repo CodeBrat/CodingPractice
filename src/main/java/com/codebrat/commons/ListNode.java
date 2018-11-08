ce6e903f00779e1b6f9ae8bacd80f8073141cca2
CodeBrat <bharathkumar.a20@gmail.com>
2018-11-07 23:25:23 -0500

    COMMONS: adding list and tree node classes

---
+++ src/main/java/com/codebrat/commons/ListNode.java
@@ -0,0 +1,13 @@
+package com.codebrat.commons;
+
+public class ListNode {
+
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+
+}
---
+++ src/main/java/com/codebrat/commons/TreeNode.java
@@ -0,0 +1,8 @@
+package com.codebrat.commons;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
