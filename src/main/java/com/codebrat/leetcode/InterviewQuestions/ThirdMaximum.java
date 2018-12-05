973ffcd6c27e9881df83c694995d9f93d4a7bceb
CodeBrat <bharathkumar.a20@gmail.com>
2018-12-04 23:02:43 -0500

    DAILY_COMMIT: updating project

---
+++ src/main/java/com/codebrat/leetcode/InterviewQuestions/ThirdMaximum.java
@@ -0,0 +1,19 @@
+package com.codebrat.leetcode.InterviewQuestions;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ThirdMaximum {
+
+    public int solution(int[] nums){
+        TreeSet<Integer> resultSet = new TreeSet<Integer>();
+        for(int num:nums){
+            resultSet.add(num);
+            if(resultSet.size() > 3){
+                resultSet.remove(resultSet.first());
+            }
+        }
+        return resultSet.last();
+    }
+
+}
